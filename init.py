36f12621bed8c965a9758c5793682e8c104ecd2e
vladimir-ascue <vladimir.ascue@gmail.com>
2023-02-15 18:22:01 -0500

    Variables y estructura

---
+++ init.py
@@ -0,0 +1,31 @@
+def operaciones():
+    apellidos = "jj"
+    def resta(m, n):
+        return m-n
+    def suma(a, b):
+        return a + b
+    print("Operacion dentro de la funcion >> ",resta(654-321))
+
+def main():
+
+    x = int(input("ingrese su primer numerito: "))
+    y = int(input("ingrese su segundo numerito: "))
+    print(suma(x, y))
+    pass
+
+main()
+
+# a, b, c = 10, 20, 30
+#
+# print(a,"-",b,"-",c)
+#
+# nombre = ""
+#
+# nombre = input("Ingrese su nombre: ")
+# print("ingrese su edad: ")
+# edad = int(input())
+#
+# print(nombre, "tiene:", (5*edad), "años") # Imprime valores separados
+#
+# print(nombre + " tiene:" , (5*edad),"años")  # Concatenar
+#
