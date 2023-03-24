490723c17407d35e44d60ab705aa9efd28a234b4
vladimir-ascue <vladimir.ascue@gmail.com>
2023-03-24 15:21:16 -0500

    Envio de datos de formulario HTML a BD

--- A04-Django/Biblioteca/Biblioteca/urls.py
+++ A04-Django/Biblioteca/Biblioteca/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 from Biblioteca.views import inicio, contacto
+from librosApp.views import crearLibro
 
 urlpatterns = [
    path('admin/', admin.site.urls),
     path('', inicio),
     path('contacto/', contacto),
-    path('clibro/', contacto),
+    path('clibro/', crearLibro),
 ]
--- A04-Django/Biblioteca/librosApp/views.py
+++ A04-Django/Biblioteca/librosApp/views.py
@@ -1,5 +1,28 @@
 from django.shortcuts import render
+from librosApp.models import libro
 
 # Create your views here.
-def contacto(request):
+def crearLibro(request):
+    if request.method == 'POST':
+        Codigo = request.POST['codigo']
+        Titulo = request.POST['titulo']
+        Autor = request.POST['autor']
+        Genero = request.POST['genero']
+        Estado = request.POST['estado']
+
+        librito = libro(codigo=Codigo, titulo=Titulo, autor=Autor, genero=Genero, estado=Estado)
+        librito.save()
+
+        return render(request, "crearLibro.html", {"titulo": Titulo})
     return render(request, "crearLibro.html")
+
+# def actiualizarLibro(request):
+#     if request.method == 'POST':
+#         Codigo = request.POST['codigo']
+#
+#         librito = libro.Objects.all(codigo = Codigo)
+#         librito.estado = "inactivo"
+#         librito.save()
+#
+#         return render(request, "actualizarLibro.html", {"titulo": Titulo})
+#     return render(request, "creaactualizarLibro.html")
